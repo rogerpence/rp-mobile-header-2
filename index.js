9e168348fabd0702378abadd53766df9a653b997
Roger Pence <roger.pence@gmail.com>
2019-07-04 12:11:44 -0500

    add new hamburger

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 autoboundEventHandlers.addEventHandler('toggleNavMenu', (e) => {
     console.log('clicked');
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('is-active');
+
     const links = document.querySelector('.links');
     links.classList.toggle('show-links');
 });
